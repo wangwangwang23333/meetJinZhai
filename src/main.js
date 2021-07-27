2728af923fb65212f7475228e12d5b298de1d948
汪汪汪 <1209497940@qq.com>
2021-07-27 19:48:01 +0800

    测试视频模块

--- src/main.js
+++ src/main.js
@@ -5,8 +5,10 @@ import store from './store'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+
 Vue.config.productionTip = false
 Vue.use(ElementUI)
+
 
 new Vue({
   router,
